e8d34ded1fc579a4e913942758b0a00fbfc64542
e.ryazanov <e.ryazanov@tbank.ru>
2025-06-23 01:00:45 +0500

    feat: убрать повторные просмотры, убрать ограничение на количество просмотров

--- test/kanban/manager/InMemoryHistoryManagerTest.java
+++ test/kanban/manager/InMemoryHistoryManagerTest.java
@@ -1,29 +1,43 @@
 package kanban.manager;
 
-import kanban.model.Epic;
-import kanban.model.Subtask;
-import kanban.model.Task;
-import kanban.model.TaskStatus;
+import kanban.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HistoryManagerTest {
+class InMemoryHistoryManagerTest {
     private HistoryManager historyManager;
     private Task testTask1;
     private Task testTask2;
+    private Task testTask3;
     private Epic testEpic;
     private Subtask testSubtask;
 
     @BeforeEach
     void beforeEach() {
-        historyManager = new HistoryManager();
+        historyManager = new InMemoryHistoryManager();
         testTask1 = new Task("Task 1", "Description", 1, TaskStatus.NEW);
         testTask2 = new Task("Task 2", "Description", 2, TaskStatus.IN_PROGRESS);
+        testTask3 = new Task("Task 3", "Description", 3, TaskStatus.IN_PROGRESS);
         testEpic = new Epic("Epic", "Description", 3);
         testSubtask = new Subtask("Subtask", "Description", 4, TaskStatus.NEW, 3);
+    }
+
+    @Test
+    void shouldRemoveTaskWhenSameTaskWasProvidedAgain() {
+        historyManager.add(testTask1);
+        historyManager.add(testTask2);
+        historyManager.add(testTask3);
+        historyManager.add(testTask2);
+        historyManager.add(testTask1);
+        List<Task> history = historyManager.getHistory();
+
+        assertEquals(3, history.size());
+        assertEquals(testTask3, history.get(0));
+        assertEquals(testTask2, history.get(1));
+        assertEquals(testTask1, history.get(2));
     }
 
     @Test
@@ -58,19 +72,6 @@ class HistoryManagerTest {
         assertEquals(testTask1, history.get(0));
         assertEquals(testTask2, history.get(1));
         assertEquals(testEpic, history.get(2));
-    }
-
-    @Test
-    void shouldNotExceedMaxSizeLimit() {
-        for (int i = 0; i < 11; i++) {
-            Task task = new Task("Task " + i, "Desc", i, TaskStatus.NEW);
-            historyManager.add(task);
-        }
-
-        List<Task> history = historyManager.getHistory();
-        assertEquals(10, history.size());
-        assertFalse(history.contains(new Task("Task 0", "Desc", 0, TaskStatus.NEW)));
-        assertTrue(history.contains(new Task("Task 10", "Desc", 10, TaskStatus.NEW)));
     }
 
     @Test
